5447de3ea492fb1da19854e1201cfec3c7100929
Kaunteya Suryawanshi <kaunteya@Kaunteys-MacBook-Pro.local>
2017-11-23 18:17:27 +0530

    Images are visible all

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 
+// <Image style={{height:25, width:25}} source={require(props.imageUri)} />
+// <Image source={{uri: props.imageUri }} />
 
 class AView extends React.Component {
   render() {
@@ -8,7 +10,10 @@ class AView extends React.Component {
       <View style={{flexDirection: 'row', paddingVertical: '2%',paddingHorizontal: '5%', borderWidth: 1}}>
         <Image
          style={{width: 50, height: 100, marginRight: '5%'}}
-         source={require('./images/iPhone.png')}
+         // source={require('./images/iPhone.png')}
+         // source={this.props.image1}
+         // source={{uri: this.props.image1}}
+         source={this.props.image1}
         />
         <View style={{paddingTop: '5%'}}>
           <Text>{this.props.name}</Text>
@@ -24,10 +29,22 @@ export default class App extends React.Component {
   onPressLearnMore() {
     console.log("Here")
   }
+
   render() {
     return (
       <View style={styles.container}>
-        <AView name = "iPhone 16 GB" brand="Apple" price="1000"/>
+        <FlatList 
+          data = {[
+            {key: "iPhone", name: "iPhone 6 (16GB, Black)", brand: "Apple", price: 399, image: require("./images/iPhone.png")},
+            {key: "s8", name: "Samsung Galaxy S8 (64GB, Black)", brand: "Samsung", price: 799, image: require("./images/samsung-s8.png")},
+            {key: "pixel", name: "Google Pixel (32GB, Very Silver)", brand: "Google", price: 699, image: require("./images/pixel.png")},
+            {key: "zte", name: "ZTE Max XL (16GB)", brand: "ZTE", price: 129, image: require("./images/zte-max.png")},
+          ]}
+
+          renderItem = {({item}) => 
+            <AView name={item.name} brand={item.brand} price={item.price} image1={item.image}/>
+          }
+        />
       </View>
     );
   }
