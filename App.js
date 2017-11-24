d86ab6787d0478a5849c7a99afd2133cc5ee9319
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2017-11-24 19:25:53 +0530

    Refactoring

--- App.js
+++ App.js
@@ -1,96 +1,33 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, FlatList } from "react-native";
-import PhoneView from "./PhoneView.js";
-
-const Dimensions = require("Dimensions");
-const windowHeight = Dimensions.get("window").height;
-const windowWidth = Dimensions.get("window").width;
-
-// const window = Dimensions.get('window');
+import Shopping from "./View/Shopping.js"
 
 export default class App extends React.Component {
-  onPressLearnMore() {
-    console.log("Here");
-  }
-  render() {
-    console.log("Rendering" + windowHeight);
 
+  render() {
     return (
-      <View style={styles.container}>
-          <FlatList style={styles.list}
-            data={phoneList}
-            renderItem={({ item }) => (
-              <PhoneView
-                name={item.name}
-                brand={item.brand}
-                price={item.price}
-                image1={item.image}
-                arrangement="list"
-              />
-            )}
-          />
-          <FlatList style={styles.grid}
-            data={phoneList}
-            numColumns={2}
-            renderItem={({ item }) => (
-              <PhoneView
-                name={item.name}
-                brand={item.brand}
-                price={item.price}
-                image1={item.image}
-                arrangement="grid"
-              />
-            )}
-          />
+      <View style={styles.app}>
+        <View style={styles.nav}>
+        </View>
+        <View style={styles.container}>
+          <Shopping/>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    flexDirection: "row",
+    flexDirection: "column",
     backgroundColor: "#fff"
   },
-  list: {
-    width: windowWidth / 2,
-    height: windowHeight,
+  nav: {
+    flex: 1,
+    backgroundColor: "#090C21"
   },
-  grid: {
-    width: windowWidth / 2,
-    height: windowHeight,
+  container: {
+    flex: 8
   }
 });
-
-const phoneList = [
-  {
-    key: "iPhone",
-    name: "iPhone 6 (16GB, Black)",
-    brand: "Apple",
-    price: 399,
-    image: require("./images/iPhone.png")
-  },
-  {
-    key: "s8",
-    name: "Samsung Galaxy S8 (64GB, Black)",
-    brand: "Samsung",
-    price: 799,
-    image: require("./images/samsung-s8.png")
-  },
-  {
-    key: "pixel",
-    name: "Google Pixel (32GB, Very Silver)",
-    brand: "Google",
-    price: 699,
-    image: require("./images/pixel.png")
-  },
-  {
-    key: "zte",
-    name: "ZTE Max XL (16GB)",
-    brand: "ZTE",
-    price: 129,
-    image: require("./images/zte-max.png")
-  }
-];
-
---
+++ View/Shopping.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import PhoneView from "../Component/PhoneView";
+
+// const Dimensions = require("Dimensions");
+// const windowHeight = Dimensions.get("window").height;
+// const windowWidth = Dimensions.get("window").width;
+
+export default class Shopping extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+          <FlatList style={styles.list}
+            data={phoneList}
+            renderItem={({ item }) => (
+              <PhoneView
+                name={item.name}
+                brand={item.brand}
+                price={item.price}
+                image1={item.image}
+                arrangement="list"
+              />
+            )}
+          />
+          <FlatList style={styles.grid}
+            data={phoneList}
+            numColumns={2}
+            renderItem={({ item }) => (
+              <PhoneView
+                name={item.name}
+                brand={item.brand}
+                price={item.price}
+                image1={item.image}
+                arrangement="grid"
+              />
+            )}
+          />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#fff"
+  },
+  list: { flex: 1, },
+  grid: { flex: 1, }
+});
+
+const phoneList = [
+  {
+    key: "iPhone",
+    name: "iPhone 6 (16GB, Black)",
+    brand: "Apple",
+    price: 399,
+    image: require("../images/iPhone.png")
+  },
+  {
+    key: "s8",
+    name: "Samsung Galaxy S8 (64GB, Black)",
+    brand: "Samsung",
+    price: 799,
+    image: require("../images/samsung-s8.png")
+  },
+  {
+    key: "pixel",
+    name: "Google Pixel (32GB, Very Silver)",
+    brand: "Google",
+    price: 699,
+    image: require("../images/pixel.png")
+  },
+  {
+    key: "zte",
+    name: "ZTE Max XL (16GB)",
+    brand: "ZTE",
+    price: 129,
+    image: require("../images/zte-max.png")
+  }
+];
+
