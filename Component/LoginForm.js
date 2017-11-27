5571d652410aecf32a1bb2a85554d49414935608
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2017-11-27 18:06:42 +0530

    .

--- Component/LoginForm.js
+++ Component/LoginForm.js
@@ -16,43 +16,51 @@ export default class LoginForm extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <ScrollView style={styles.form}>
-        <View style={styles.inputGroup}>
-          <Text style={styles.label}>Email</Text>
-          <TextInput style={styles.input} editable />
-        </View>
-        <View style={styles.inputGroup}>
-          <Text style={styles.label}>Password</Text>
-          <TextInput style={styles.input} secureTextEntry editable />
-          <Text style={styles.secondaryLabel}>Forgot Password?</Text>
-        </View>
-        <View style={styles.inputGroup}>
-        <Button
-            title="Login"
-            color="#27AE60"
-            click={() => this.login()}
-          />
-          <Text style={[styles.secondaryLabel, { textAlign: "center", paddingVertical: "3%"}]}>
-            or
-          </Text>
-          <Button
-            title="Login with Facebook"
-            color="#1D5196"
-            click={() => this.login()}
-          />
-        </View>
-        <View style={styles.inputGroup}>
-          <Text style={[styles.secondaryLabel, { textAlign: "center" }]}>
-            Sign up for a new account
-          </Text>
-        </View>
-        <View style={styles.inputGroup}>
-          <Button
-            title="Skip and Continue  ➔"
-            color="#AAA"
-            click={() => this.login()}
-          />
-        </View>
+        <ScrollView style={styles.form}>
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Email</Text>
+            <TextInput style={styles.input} editable />
+          </View>
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Password</Text>
+            <TextInput style={styles.input} secureTextEntry editable />
+            <Text style={styles.secondaryLabel}>Forgot Password?</Text>
+          </View>
+          <View style={styles.inputGroup}>
+            <Button title="Login" color="#27AE60" click={() => this.login()} />
+
+            {this.props.socialMedia && (
+              <Text
+                style={[
+                  styles.secondaryLabel,
+                  { textAlign: "center", paddingVertical: "3%" }
+                ]}
+              >
+                or
+              </Text>
+            )}
+            {this.props.socialMedia && (
+              <Button
+                title="Login with Facebook"
+                color="#1D5196"
+                click={() => this.login()}
+              />
+            )}
+          </View>
+          <View style={styles.inputGroup}>
+            <Text style={[styles.secondaryLabel, { textAlign: "center" }]}>
+              Sign up for a new account
+            </Text>
+          </View>
+          {this.props.skip && (
+          <View style={styles.inputGroup}>
+            <Button
+              title="Skip and Continue  ➔"
+              color="#AAA"
+              click={() => this.login()}
+            />
+          </View>
+          )}
         </ScrollView>
       </View>
     );
@@ -64,12 +72,11 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
     borderWidth: StyleSheet.hairlineWidth,
-    backgroundColor: "#fff"    
+    backgroundColor: "#fff"
   },
   form: {
     marginHorizontal: "10%",
-    marginVertical: "5%",
-
+    marginVertical: "5%"
   },
   inputGroup: {
     marginVertical: "4%"
